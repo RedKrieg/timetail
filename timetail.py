3eeffc67a2bce4914cff61e94c5b03ef0c4baed1
brandonw <brandonw@inmotionhosting.com>
2012-04-17 03:12:09 -0400

    started python code to build a "can this position be this type" table

---
+++ timetail.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+import re
+with open("log.log") as f:
+    data=f.read()
+
+def parse_data(data):
+    """Parses all data in the chunk, returning, you know, stuff."""
+    # Unlikely we'll find 20 significant numbers before our date is complete
+    shortest = 20
+    
+    position_map = {
+        'year': True,
+        'month': True,
+        'day': True,
+        'hour': True,
+        'minute': True,
+        'second': True
+    }
+
+    positions = []
+
+    for i in xrange(shortest):
+        positions.append(position_map.copy())
+
+    month_map = {
+        'Jan': 1,
+        'Feb': 2,
+        'Mar': 3,
+        'Apr': 4,
+        'May': 5,
+        'Jun': 6,
+        'Jul': 7,
+        'Aug': 8,
+        'Sep': 9,
+        'Oct': 10,
+        'Nov': 11,
+        'Dec': 12
+    }
+
+    value_regex = re.compile("|".join(["\d+"] + month_map.keys())) 
+
+    # We remove the first line because it may not be complete.
+    for line in data.splitlines()[1:]:
+        values = value_regex.findall(line)
+        
+        if len(values) < shortest:
+            shortest = len(values)
+            positions = positions[:shortest]
+
+        for position in xrange(len(positions)):
+            # Convert month names to values
+            if re.match('[a-zA-Z]+', values[position]):
+                values[position] = month_map[values[position]]
+            value = values[position]
+
+            # Cascade down our exclusion list.  Add exclusions to the next match
+            if value > 3000:
+                for unit in positions[position]:
+                    positions[position][unit] = False
+            elif value > 60:
+                for unit in positions[position]:
+                    if unit != 'year':
+                        positions[position][unit] = False
+            elif value > 31:
+                for unit in positions[position]:
+                    if not re.match('year|minute|second', unit):
+                        positions[position][unit] = False
+            elif value > 24:
+                for unit in positions[position]:
+                    if not re.match('year|day|minute|second', unit):
+                        positions[position][unit] = False
+            elif value > 12:
+                for unit in positions[position]:
+                    if not re.match('year|day|hour|minute|second', unit):
+                        positions[position][unit] = False
+
+        print values
+
+    print shortest
+    print positions
+
+
+
+
+
+parse_data(data)
